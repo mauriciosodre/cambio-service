e0c688bdff176cd7f80f08f32364a26dfb67faa8
msodrejr <m.sodre8@gmail.com>
2022-03-25 01:28:54 -0300

    Usando environment para pegar a porta do service, e add jpa e flyway dependencies

--- src/main/java/com/msodre/cambioservice/controller/CambioController.java
+++ src/main/java/com/msodre/cambioservice/controller/CambioController.java
@@ -1,6 +1,8 @@
 package com.msodre.cambioservice.controller;
 
 import com.msodre.cambioservice.model.Cambio;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/cambio-service")
 public class CambioController {
 
+  private final Environment environment;
+
   @GetMapping("/{amount}/{from}/{to}")
   public Cambio getCambio(
       @PathVariable BigDecimal amount, @PathVariable String from, @PathVariable String to) {
+    var port = environment.getProperty("local.server.port");
     return Cambio
             .builder()
             .id(1L)
@@ -22,7 +28,7 @@ public class CambioController {
             .to(to)
             .conversionFactor(BigDecimal.ONE)
             .convertedValue(BigDecimal.ONE)
-            .environment("PORT 8000")
+            .environment("PORT " + port)
             .build();
   }
 }
